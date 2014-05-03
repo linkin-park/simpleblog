04fcd8dd85a5c5bd9bfc84f25e9441d3c582e91e
tlotr <tlotr@users.noreply.github.com>
2014-05-03 23:24:07 +0530

    Update edit_post.php

--- edit_post.php
+++ edit_post.php
@@ -34,9 +34,37 @@ if(isset ($_POST['title'], $_POST['contents']) ){
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title> </title>
+<style>
+
+body { background-color: #000000; color: #ffffff; font-family: "lucida sans unicode"; font-size: 14px }
+
+div#p_post { border: 0px solid gold; padding: 10px }
+
+a:link { color: green; text-decoration: none }
+a:active { color: red; text-decoration: none }
+a:visited { color: green; text-decoration: none }
+a:hover { color: orange; text-decoration: none }
+
+span#p_date { color: orange; font-weight: bold }
+
+input { background-color: #1E1E1E; color: white; border: 0; padding: 5px; }
+textarea { background-color: #1E1E1E; color: white; border: 0; padding-left: 5px; padding-top: 10px; padding-right: 10px; padding-bottom: 5px; overflow: auto; }
+#submit:hover { background-color: #5C5C5C; color: orange }
+
+ul#topnav { list-style-type: none; }
+li.top-nav { display: inline; margin-right: 20px; }
+
+#title { width: 370px; font-family: "lucida sans unicode" }
+#contents { width: 365px; height: 300px; font-family: "lucida sans unicode" }
+#submit { padding: 2px }
+
+</style>
+<title><?php echo $post[0]['title']; ?></title>
 </head>
 <body>
+<ul id="topnav">
+	<li class="top-nav"><a href="index.php">Home</a></li>
+</ul>
 <h1>Edit This post</h1>
 
 <?php
@@ -47,12 +75,19 @@ if(isset($errors) && !empty($errors)){
 
 ?>
 <form method="post" action="">
-<label for="title">Title</label>
-<input type="text" name="title" value="<?php echo $post[0]['title']; ?>" />
-<br /><br />
-<textarea name="contents" rows="15" cols="50"><?php echo $post[0]['contents']; ?></textarea>
-<br /><br />
-<input type="submit" value="Submit" name="submit" />
+<table border="0" cellspacing="5" cellpadding="5">
+<tr>
+<td><label for="title">Title<font color="red">*</font>: </label></td>
+<td><input type="text" name="title" id="title" autocomplete="off" value="<?php echo $post[0]['title']; ?>" /></td>
+</tr>
+<tr>
+<td><label for="contents">Contents<font color="red">*</font>: </label></td>
+<td><textarea name="contents" id="contents"><?php echo $post[0]['contents']; ?></textarea></td>
+</tr>
+<tr>
+<td colspan="2"><input type="submit" id="submit" value="Add Post" name="submit" /></td>
+</tr>
+</table>
 </form>
 
 </body>
