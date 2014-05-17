a5972babf4af54d54682f88298353f31d951ab63
tlotr <tlotr@users.noreply.github.com>
2014-05-17 22:57:20 +0530

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2014 at 11:22 PM
+-- Generation Time: May 17, 2014 at 10:55 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -23,20 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attachments`
---
-
-CREATE TABLE IF NOT EXISTS `attachments` (
-  `attach_id` int(6) NOT NULL AUTO_INCREMENT,
-  `id` int(6) NOT NULL,
-  `names` text NOT NULL,
-  PRIMARY KEY (`attach_id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `posts`
 --
 
@@ -45,18 +31,26 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `title` varchar(255) NOT NULL,
   `contents` text NOT NULL,
   `posted_date` datetime DEFAULT '0000-00-00 00:00:00',
+  `names` varchar(255) NOT NULL,
+  `username` varchar(25) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Constraints for dumped tables
+-- Table structure for table `users`
 --
 
---
--- Constraints for table `attachments`
---
-ALTER TABLE `attachments`
-  ADD CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`id`) REFERENCES `posts` (`id`);
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `f_name` varchar(30) NOT NULL,
+  `l_name` varchar(30) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
